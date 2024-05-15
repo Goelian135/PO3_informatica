e43eac5716febf6f8d85206ef31dac2616b9451c
Julian van Kuik <167704798+Goelian135@users.noreply.github.com>
2024-05-15 19:17:14 +0200

    Commentaar voor alle documenten

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = function() {
   togglePage('home');
 };
+/* Deze functie zorgt ervoor dat de togglePage functie meteen afgaat bij het laden van de pagina
+   Met het id 'home' zodat, zodra je de website laad, alleen home zichtbaar is */
 
 function togglePage(pageId)  {
   var pages = document.querySelectorAll('.page');
@@ -16,3 +18,10 @@ function togglePage(pageId)  {
     }
   });
 }
+
+/* Deze functie maakt alle divs met class page onzichtbaar 
+  behalve degene met hetzelfde id die is meegegeven. Dit doet hij met de volgende stappen:
+  1. Het pageId dat hij heeft meegekregen in de var pageId doen
+  2. De var pages aanmaken en alle divs met de class 'page' verzamelen
+  3. Voor elke id met class 'page' kijken of het id overeenkomt met pageId. 
+     Zo ja, dan word de div zichtbaar. Zo niet, dan word de div onzichtbaar.*/
